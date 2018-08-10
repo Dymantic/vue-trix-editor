52ae38f6f64ec302e267f3c3b3749e16b1883742
Michael Joyner <joyner.michael@gmail.com>
2018-08-10 11:44:55 +0800

    add content prop

--- src/components/TrixVueWysiwyg.vue
+++ src/components/TrixVueWysiwyg.vue
@@ -1,8 +1,20 @@
 <template>
-    <div></div>
+    <div>
+        <trix-editor input="dd-trix-editor-input"></trix-editor>
+        <input type="text" id="dd-trix-editor-input" name="" v-model="content">
+    </div>
 </template>
 
 <script>
-export default {};
+import Trix from "trix";
+
+export default {
+  props: {
+    content: {
+      type: String,
+      required: true
+    }
+  }
+};
 </script>
 
--- tests/unit/TrixVueWysiwyg.spec.js
+++ tests/unit/TrixVueWysiwyg.spec.js
@@ -1,9 +1,24 @@
 import { shallowMount } from "@vue/test-utils";
 import TrixVueWysiwyg from "@/components/TrixVueWysiwyg.vue";
 
-describe("HelloWorld.vue", () => {
+describe("TrixVueWysiwyg.vue", () => {
   it("passes a test", () => {
-    const wrapper = shallowMount(TrixVueWysiwyg);
+    const wrapper = shallowMount(TrixVueWysiwyg, {
+      propsData: {
+        content: "<h1>Test It!</h1>"
+      }
+    });
     expect(wrapper.text()).toMatch("");
+  });
+
+  it("uses the content prop in the trix editor", () => {
+    const wrapper = shallowMount(TrixVueWysiwyg, {
+      propsData: {
+        content: "<h1>Test It!</h1>"
+      }
+    });
+    const editor = wrapper.find("#dd-trix-editor-input");
+
+    expect(editor.element.value).toBe("<h1>Test It!</h1>");
   });
 });
