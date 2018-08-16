56deeacd9911bd313d584098466020610c6bfd74
Michael Joyner <joyner.michael@gmail.com>
2018-08-16 09:51:33 +0800

    start with file attachments

--- src/components/TrixVueWysiwyg.vue
+++ src/components/TrixVueWysiwyg.vue
@@ -1,19 +1,55 @@
 <template>
     <div>
-        <trix-editor input="dd-trix-editor-input"></trix-editor>
-        <input type="text" id="dd-trix-editor-input" name="" v-model="content">
+        <trix-editor @trix-file-accept="handleFile" ref="editor" :input="uniqueId"></trix-editor>
+        <input type="text" :id="uniqueId" name="" :value="initialContent">
     </div>
 </template>
 
 <script>
+//eslint-disable-next-line
 import Trix from "trix";
+// import _ from "trix/dist/trix.css";
 
 export default {
   props: {
-    content: {
+    initialContent: {
       type: String,
-      required: true
+      default: ""
+    },
+    uniqueId: {
+      type: String,
+      default: "dd-trix-input"
+    },
+    imageUploadPath: {
+      type: String
     }
+  },
+
+  mounted() {
+    this.$refs.editor.addEventListener("trix-initialize", this.init);
+  },
+
+  watch: {
+    content(n) {
+      this.$refs.editor.editor.insertHTML(n);
+    }
+  },
+
+  methods: {
+    init() {
+      this.$refs.editor.addEventListener("trix-change", () => {
+        this.$emit("input", this.content);
+      });
+    },
+
+    handleFile(ev) {
+      if (!this.imageUploadPath) {
+        return ev.preventDefault();
+      }
+      this.processFile(ev.file);
+    },
+
+    processFile(file) {}
   }
 };
 </script>
--- tests/unit/TrixVueWysiwyg.spec.js
+++ tests/unit/TrixVueWysiwyg.spec.js
@@ -1,24 +1,93 @@
 import { shallowMount } from "@vue/test-utils";
 import TrixVueWysiwyg from "@/components/TrixVueWysiwyg.vue";
+import sinon from "sinon";
 
-describe("TrixVueWysiwyg.vue", () => {
-  it("passes a test", () => {
-    const wrapper = shallowMount(TrixVueWysiwyg, {
-      propsData: {
-        content: "<h1>Test It!</h1>"
-      }
-    });
-    expect(wrapper.text()).toMatch("");
+describe("the TrixVue component", () => {
+  it("has a default value for the unique id and input name for the trix editor", () => {
+    const wrapper = shallowMount(TrixVueWysiwyg);
+    const editor_input = wrapper.find("#dd-trix-input");
+    const editor = wrapper.find("trix-editor");
+
+    expect(editor_input.element).toBeDefined();
+    expect(editor_input.element.tagName).toBe("INPUT");
+
+    expect(editor.element.getAttribute("input")).toBe("dd-trix-input");
   });
 
-  it("uses the content prop in the trix editor", () => {
+  it("uses the initial content prop to populate the trix editor", () => {
     const wrapper = shallowMount(TrixVueWysiwyg, {
       propsData: {
-        content: "<h1>Test It!</h1>"
+        initialContent: "<h1>Test It!</h1>"
       }
     });
-    const editor = wrapper.find("#dd-trix-editor-input");
+    const editor = wrapper.find("#dd-trix-input");
 
     expect(editor.element.value).toBe("<h1>Test It!</h1>");
+  });
+
+  it("can take the unique id and input name for the trix editor as a prop", () => {
+    const wrapper = shallowMount(TrixVueWysiwyg, {
+      propsData: {
+        uniqueId: "unique-test-id"
+      }
+    });
+    const editor_input = wrapper.find("#unique-test-id");
+    const editor = wrapper.find("trix-editor");
+
+    expect(editor.element).toBeDefined();
+    expect(editor.element.getAttribute("input")).toBe("unique-test-id");
+  });
+
+  it("rejects file attachments if no image path prop is given", () => {
+    const wrapper = shallowMount(TrixVueWysiwyg, {
+      attachToDocument: true
+    });
+    const ev = {
+      preventDefault: sinon.fake()
+    };
+    const editor = wrapper.find("trix-editor");
+
+    editor.trigger("trix-file-accept", ev);
+
+    expect(ev.preventDefault.calledOnce).toBe(true);
+  });
+
+  it("it can process an attached file if there is an image upload path", () => {
+    const wrapper = shallowMount(TrixVueWysiwyg, {
+      attachToDocument: true,
+      propsData: {
+        imageUploadPath: "/test-upload"
+      }
+    });
+    wrapper.setMethods({ processFile: sinon.stub() });
+    const ev = {
+      preventDefault: sinon.fake(),
+      file: {
+        type: "text/html"
+      }
+    };
+    const editor = wrapper.find("trix-editor");
+
+    editor.trigger("trix-file-accept", ev);
+
+    expect(ev.preventDefault.calledOnce).toBe(false);
+    expect(wrapper.vm.processFile.calledOnce).toBe(true);
+  });
+
+  it("rejects file attachments if they are not images", () => {
+    const wrapper = shallowMount(TrixVueWysiwyg, {
+      attachToDocument: true
+    });
+    const ev = {
+      preventDefault: sinon.fake(),
+      file: {
+        type: "text/html"
+      }
+    };
+    const editor = wrapper.find("trix-editor");
+
+    editor.trigger("trix-file-accept", ev);
+
+    expect(ev.preventDefault.calledOnce).toBe(true);
   });
 });
